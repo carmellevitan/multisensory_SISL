77129ee833dd830b43eb9e4e94cece912cfeabc8
Aled Cuda <aledvirgil@gmail.com>
2018-07-16 14:43:48 -0700

    generate files in the correct format

--- generator/PPTWebCreator.m
+++ generator/PPTWebCreator.m
@@ -1,14 +1,26 @@
 function PPTWebCreator(PPT)
+  if randi(2) == 1
+    TestFileMaker(strcat(s(PPT), "-1"), -1, true)
+    load(strcat("../PPTFile/",s(PPT), "-1" , "_PPTFile"));
+    TestFileMaker(strcat(s(PPT), "-2"), num, false)
+  else
+    TestFileMaker(strcat(s(PPT), "-1"), -1, false)
+    load(strcat("../PPTFile/",s(PPT), "-1" , "_PPTFile"));
+    TestFileMaker(strcat(s(PPT), "-2"), num, true)
+  end
+end
+
+function TestFileMaker(PPTStr, Avoid, sound)
   global rowid;
   rowid = -1;
-  enable_sound = true;
-  outfile = fopen(strcat("../PPTFile/", int2str(PPT), ".csv"), 'W');
-  PPTFileCreator(PPT);
-  load(strcat("../PPTFile/", int2str(PPT), "_PPTFile"));
+  enable_sound = sound;
+  outfile = fopen(strcat("../PPTFile/", PPTStr, ".csv"), 'W');
+  PPTFileCreator(PPTStr, Avoid);
+  load(strcat("../PPTFile/", PPTStr, "_PPTFile"));
 
   pposarr = [1,3,2,4,2,3,1,4,3,2,1,4,3,4,1,2,1,3,4,2,1,3,2,4];
   plenarr = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2];
-  pcatarr = [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3];
+  pcatarr = [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4];
 
   MKHeader(outfile);
   MKSequence(outfile, pposarr, plenarr, pcatarr, enable_sound);
@@ -19,16 +31,49 @@ function PPTWebCreator(PPT)
 
   MKSequence(outfile, Level1Order, Level1ISIs, Level1Noise, enable_sound);
   MKDialog(outfile, "dialog3", 2500);
-  MKSequence(outfile, Level1Order, Level2ISIs, Level2Noise, enable_sound);
+  SetScore(outfile, -1);
+  MKSequence(outfile, Level2Order, Level2ISIs, Level2Noise, enable_sound);
   MKDialog(outfile, "dialog3", 2500);
-  MKSequence(outfile, Level1Order, Level3ISIs, Level3Noise, enable_sound);
+  SetScore(outfile, -1);
+  MKSequence(outfile, Level3Order, Level3ISIs, Level3Noise, enable_sound);
   MKDialog(outfile, "dialog3", 2500);
-  MKSequence(outfile, Level1Order, Level4ISIs, Level4Noise, enable_sound);
+  SetScore(outfile, -1);
+  MKSequence(outfile, Level4Order, Level4ISIs, Level4Noise, enable_sound);
   MKDialog(outfile, "dialog3", 2500);
-  MKSequence(outfile, Level1Order, Level5ISIs, Level5Noise, enable_sound);
+  SetScore(outfile, -1);
+  MKSequence(outfile, Level5Order, Level5ISIs, Level5Noise, enable_sound);
   MKDialog(outfile, "dialog3", 2500);
-  MKSequence(outfile, Level1Order, Level6ISIs, Level6Noise, enable_sound);
+  SetScore(outfile, -1);
+  MKSequence(outfile, Level6Order, Level6ISIs, Level6Noise, enable_sound);
   MKDialog(outfile, "dialog3", 2500);
+  SetScore(outfile, -1);
+  MKSequence(outfile, Level7Order, Level7ISIs, Level7Noise, enable_sound);
+  MKDialog(outfile, "dialog3", 2500);
+  SetScore(outfile, -1);
+  MKSequence(outfile, Level8Order, Level8ISIs, Level8Noise, !enable_sound);
+  MKDialog(outfile, "dialog4", 2500);
+  SetScore(outfile, -1);
+  MKDialog(outfile, "dialog5", 2500);
+  SetSpeed(outfile, 0);
+
+  recorder = randperm(5);
+  for i = recorder
+    MKDialog(outfile, "dialog6", 500);
+    switch i
+      case 1
+	MKSequence(outfile, Post1Order, Post1ISIs, ones(1, length(Post1Order)) * 0, enable_sound)
+      case 2
+	MKSequence(outfile, Post2Order, Post2ISIs, ones(1, length(Post2Order)) * 2, enable_sound)
+      case 3
+	MKSequence(outfile, Post3Order, Post3ISIs, ones(1, length(Post3Order)) * 3, !enable_sound)
+      case 4
+	MKSequence(outfile, Post4Order, Post4ISIs, ones(1, length(Post4Order)) * 1, enable_sound)
+      case 5
+	MKSequence(outfile, Post5Order, Post5ISIs, ones(1, length(Post5Order)) * 1, !enable_sound)
+    end
+    MKDialog(outfile, "dialog7", 5000);
+  end
+  MKDialog(outfile, "dialog8", 2500);
 
   fclose(outfile);
 end
@@ -52,11 +97,15 @@ function MKFall(fd, value, sound, length, category)
   if(sound); type_string = "cue"; else; type_string = "off"; end % why cant we have a ternary operator?
   switch category
     case 0
-      cat = "s";
+      cat = "sequence";
     case 1
-      cat = "n";
+      cat = "noise";
+    case 2
+      cat = "impfoil1";
     case 3
-      cat = "p";
+      cat = "impfoil2";
+    case 4
+      cat = "practice";
   end
   
   l = length * 350;
